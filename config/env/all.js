f8b5c5fadba25c03f4e237ecd8bf91789be8a63e
Kevin O'Neil <kevinmoneil@me.com>
2015-02-07 23:50:22 +0900

    added jquery files

--- config/env/all.js
+++ config/env/all.js
@@ -15,6 +15,7 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'public/lib/angular-ui-calendar/fullcalendar/fullcalendar.css'
 			],
 			js: [
 				'public/lib/angular/angular.js',
@@ -25,7 +26,11 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+        'public/lib/angular-ui-calendar/jquery/dist/jquery.js',
+        'public/lib/angular-ui-calendar/jquery-ui/ui/jquery-ui.js',
+				'public/lib/angular-ui-calendar/fullcalendar/fullcalendar.js',
+				'public/lib/angular-ui-calendar/src/calendar.js'
 			]
 		},
 		css: [
